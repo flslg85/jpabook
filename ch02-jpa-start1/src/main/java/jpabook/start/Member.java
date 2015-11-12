b02b9d1c3d06015c932f2d5b3a61d43b6b058017
jaehoon.jeong <ivory@wemakeprice.com>
2015-11-12 17:36:59 +0900

    1. member 클래스의 요구 사항 추가

--- ch02-jpa-start1/src/main/java/jpabook/start/Member.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Member.java
@@ -1,10 +1,16 @@
 package jpabook.start;
 
 import javax.persistence.*;  //**
+import java.util.Date;
 
 /**
  * User: HolyEyE
  * Date: 13. 5. 24. Time: 오후 7:43
+ *
+ * 요구사항 추가
+ * 1. 회원은 일반 회원과 관리자로 구분해야 한다
+ * 2. 회원 가입일과 수정일이 있어야 한다
+ * 3. 회원을 설명할 수 있는 필드가 있어야 한다( 이 필드는 길이 제한이 없다 )
  */
 @Entity
 @Table(name="MEMBER")
@@ -18,6 +24,21 @@ public class Member {
     private String username;
 
     private Integer age;
+
+    // 추가
+    @Enumerated(EnumType.STRING)    // 자바의 enum을 사용하려면 @Enumerated 어노테이션으로 매핑해야 한다
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)   // 날짜 타입을 매핑
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob    // BLOB, CLOB 타입을 매핑 / 회원을 설명하는 필드는 길이 제한이 없으므로 CLOB 타입으로 저장해야 함
+    private String description;
+
+
 
     public String getId() {
         return id;
---
+++ ch02-jpa-start1/src/main/java/jpabook/start/RoleType.java
@@ -0,0 +1,8 @@
+package jpabook.start;
+
+/**
+ * Created by we on 2015. 11. 12..
+ */
+public enum RoleType {
+    ADMIN, USER
+}
