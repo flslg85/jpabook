4dbd6b9ad90a37d0c65e979fd342fa261e6af6f2
jaehoon.jeong <ivory@wemakeprice.com>
2015-11-13 12:02:06 +0900

    SequenceGenerator 위치 변경

--- ch02-jpa-start1/src/main/java/jpabook/start/Board.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Board.java
@@ -6,16 +6,6 @@ import javax.persistence.*;
  * Created by flslg85 on 2015. 11. 12..
  */
 @Entity
-/**
- * sequence
- * @SequenceGenerator를 사용해서 BOARD_SEQ_GENERATOR 시퀀스 생성기를 등록
- */
-//@SequenceGenerator(
-//        name = "BOARD_SEQ_GENERATOR",
-//        sequenceName = "BOARD_SEQ", // 매핑할 데이터베이스 시퀀스 이름
-//        initialValue = 1,
-//        allocationSize = 1
-//)
 public class Board {
     @Id
     /**
@@ -27,11 +17,18 @@ public class Board {
     /**
      * sequence
      * 오라클, PostgreSQL, DB2, H2 에서 사용
+     * @SequenceGenerator를 사용해서 BOARD_SEQ_GENERATOR 시퀀스 생성기를 등록
      */
 //    @GeneratedValue(
 //            strategy = GenerationType.SEQUENCE,   // 키 생성 전략을 설정
 //            generator = "BOARD_SEQ_GENERATOR"     // 등록한 시퀀스 생성기를 선택
 //    )
+//    @SequenceGenerator(
+//        name = "BOARD_SEQ_GENERATOR",
+//        sequenceName = "BOARD_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+//        initialValue = 1,
+//        allocationSize = 1
+//)
     private Long id;
 
     private String data;
