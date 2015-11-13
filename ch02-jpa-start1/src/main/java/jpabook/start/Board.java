c519838337727bedbd68927e165a2b63b275ff81
jaehoon.jeong <ivory@wemakeprice.com>
2015-11-13 11:41:18 +0900

    sequence 로 기본키 추가

--- ch02-jpa-start1/src/main/java/jpabook/start/Board.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Board.java
@@ -6,9 +6,31 @@ import javax.persistence.*;
  * Created by flslg85 on 2015. 11. 12..
  */
 @Entity
+/**
+ * sequence
+ */
+//@SequenceGenerator(
+//        name = "BOARD_SEQ_GENERATOR",
+//        sequenceName = "BOARD_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+//        initialValue = 1,
+//        allocationSize = 1
+//)
 public class Board {
     @Id
+    /**
+     * identity
+     * MySQL, PostgreSQL, SQL Server, DB2 에서 사용
+     */
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    /**
+     * sequence
+     * 오라클, PostgreSQL, DB2, H2 에서 사용
+     */
+//    @GeneratedValue(
+//            strategy = GenerationType.SEQUENCE,
+//            generator = "BOARD_SEQ_GENERATOR"
+//    )
     private Long id;
 
     private String data;
